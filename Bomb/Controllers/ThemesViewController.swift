9ab624201da74ab19b8c274a48fa85e6536f9c14
DanilaOkuneu <okuneu.accounts@icloud.com>
2023-08-07 14:21:20 +0300

    add project files

---
+++ Bomb/Controllers/ThemesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThemesViewController.swift
+//  Bomb
+//
+//  Created by Danila Okuneu on 7.08.23.
+//
+
+import UIKit
+
+class ThemesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bomb/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bomb
+//
+//  Created by Danila Okuneu on 7.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
